d4ff71306373bd784d9c23a7fa3db9a721cad6bb
hyohyeon08 <hyohyeonkim08@gmail.com>
2026-04-21 15:19:57 +0900

    [level 2] Title: 모음 사전, Time: 1.72 ms, Memory: 9.84 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,21 @@
+alpha = ['A', 'E', 'I', 'O', 'U']
+result = []
+
+def dfs(word):
+    result.append(word) 
+    
+    if len(word) == 5:
+        return
+    
+    for ch in alpha:
+        dfs(word + ch)
+
+def solution(word):
+    for ch in alpha:
+        dfs(ch)
+    
+    return result.index(word) + 1 
+
+word = "AA"
+answer = solution(word)
+print(answer)
